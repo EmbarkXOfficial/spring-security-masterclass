6a59c81931ad7db18a3c40b4e8c9eaa3b9d7996e
Faisal Memon <faisal.memon1991@gmail.com>
2024-06-02 21:35:49 +0530

    sec-7-set-spring-project

---
+++ src/main/java/com/secure/notes/NotesApplication.java
@@ -0,0 +1,13 @@
+package com.secure.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
