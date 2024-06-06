fe285bd145e1a85166bcbb56e64f591355106be5
Faisal Memon <faisal.memon1991@gmail.com>
2024-06-06 15:12:01 +0530

    sec-32-deny-certain-requests

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig {
                 requests
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/admin").denyAll()
+                        .requestMatchers("/admin/**").denyAll()
                         .anyRequest().authenticated());
         // http.formLogin(withDefaults());
         http.sessionManagement(session ->
