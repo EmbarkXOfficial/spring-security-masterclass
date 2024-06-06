cfb514192a72520f13808d9d84c20f69b4ac88be
Faisal Memon <faisal.memon1991@gmail.com>
2024-06-06 19:37:08 +0530

    sec-36-set-notes-controller

---
+++ src/main/java/com/secure/notes/controllers/NoteController.java
@@ -0,0 +1,48 @@
+package com.secure.notes.controllers;
+
+import com.secure.notes.models.Note;
+import com.secure.notes.services.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+    @Autowired
+    private NoteService noteService;
+
+    @PostMapping
+    public Note createNote(@RequestBody String content,
+                           @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        System.out.println("USER DETAILS: " + username);
+        return noteService.createNoteForUser(username, content);
+    }
+
+    @GetMapping
+    public List<Note> getUserNotes(@AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        System.out.println("USER DETAILS: " + username);
+        return noteService.getNotesForUser(username);
+    }
+
+    @PutMapping("/{noteId}")
+    public Note updateNote(@PathVariable Long noteId,
+                           @RequestBody String content,
+                           @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        return noteService.updateNoteForUser(noteId, content, username);
+    }
+
+    @DeleteMapping("/{noteId}")
+    public void deleteNote(@PathVariable Long noteId,
+                           @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        noteService.deleteNoteForUser(noteId, username);
+    }
+}
