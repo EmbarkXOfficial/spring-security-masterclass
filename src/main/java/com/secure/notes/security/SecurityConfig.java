896172735372e3d982b29d574b97f590cdde013c
Faisal Memon <faisal.memon1991@gmail.com>
2024-06-10 12:53:17 +0530

    sec-42-testing-api

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers("/admin/**").denyAll()
                         .anyRequest().authenticated());
         // http.formLogin(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
