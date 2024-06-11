82d7b421ade37710a1764e9ee9250ba85cdba9c8
Faisal Memon <faisal.memon1991@gmail.com>
2024-06-11 13:59:27 +0530

    sec-56-core-classes

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,9 +30,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager =
-                new InMemoryUserDetailsManager();
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager manager =
+                new JdbcUserDetailsManager(dataSource);
         if (!manager.userExists("user1")) {
             manager.createUser(
                     User.withUsername("user1")
