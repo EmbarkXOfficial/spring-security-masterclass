a662ed86570f249660a3e083e76861d275d04ab1
Faisal Memon <faisal.memon1991@gmail.com>
2024-07-09 12:31:02 +0530

    Implementing Models and Repositories

---
+++ src/main/java/com/secure/notes/models/AuditLog.java
@@ -0,0 +1,22 @@
+package com.secure.notes.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String action;
+    private String username;
+    private Long noteId;
+    private String noteContent;
+    private LocalDateTime timestamp;
+}
