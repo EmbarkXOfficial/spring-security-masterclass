c8451ecc63eb1d2eb9f69d65485e33dd9a44d1d4
Faisal Memon <faisal.memon1991@gmail.com>
2024-07-23 13:12:51 +0530

    Testing the Entire Flow with Front End | Loose Ends

--- src/main/java/com/secure/notes/config/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/secure/notes/config/OAuth2LoginSuccessHandler.java
@@ -23,9 +23,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -116,6 +114,13 @@ public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSu
         String email = (String) attributes.get("email");
         System.out.println("OAuth2LoginSuccessHandler: " + username + " : " + email);
 
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>(oauth2User.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
+                .collect(Collectors.toList()));
+        User user = userService.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("User not found"));
+        authorities.add(new SimpleGrantedAuthority(user.getRole().getRoleName().name()));
+
         // Create UserDetailsImpl instance
         UserDetailsImpl userDetails = new UserDetailsImpl(
                 null,
@@ -123,9 +128,7 @@ public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSu
                 email,
                 null,
                 false,
-                oauth2User.getAuthorities().stream()
-                        .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
-                        .collect(Collectors.toList())
+                authorities
         );
 
         // Generate JWT token
--- src/main/java/com/secure/notes/security/jwt/JwtUtils.java
+++ src/main/java/com/secure/notes/security/jwt/JwtUtils.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtils {
@@ -35,8 +36,12 @@ public class JwtUtils {
 
     public String generateTokenFromUsername(UserDetails userDetails) {
         String username = userDetails.getUsername();
+        String roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.joining(","));
         return Jwts.builder()
                 .subject(username)
+                .claim("roles", roles)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key())
